04f84ec6d35c1295a199f34d9b7f48bacf53fafa
Utrit <Vadyasrus@gmail.com>
2019-02-27 21:49:47 +0500

    v0.0.1

--- ASort/Form1.Designer.cs
+++ ASort/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ASort
 {
-    partial class Form1
+    partial class ASort
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SelectB = new System.Windows.Forms.Button();
+            this.PathBox = new System.Windows.Forms.TextBox();
+            this.ConvertTypes = new System.Windows.Forms.RichTextBox();
+            this.SortB = new System.Windows.Forms.Button();
+            this.FolderDial = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // SelectB
+            // 
+            this.SelectB.Location = new System.Drawing.Point(274, 12);
+            this.SelectB.Name = "SelectB";
+            this.SelectB.Size = new System.Drawing.Size(98, 29);
+            this.SelectB.TabIndex = 0;
+            this.SelectB.Text = "Select";
+            this.SelectB.UseVisualStyleBackColor = true;
+            this.SelectB.Click += new System.EventHandler(this.SelectB_Click);
+            // 
+            // PathBox
+            // 
+            this.PathBox.Location = new System.Drawing.Point(12, 17);
+            this.PathBox.Name = "PathBox";
+            this.PathBox.Size = new System.Drawing.Size(251, 20);
+            this.PathBox.TabIndex = 1;
+            // 
+            // ConvertTypes
+            // 
+            this.ConvertTypes.Location = new System.Drawing.Point(12, 52);
+            this.ConvertTypes.Name = "ConvertTypes";
+            this.ConvertTypes.Size = new System.Drawing.Size(251, 201);
+            this.ConvertTypes.TabIndex = 2;
+            this.ConvertTypes.Text = "";
+            // 
+            // SortB
+            // 
+            this.SortB.Location = new System.Drawing.Point(274, 52);
+            this.SortB.Name = "SortB";
+            this.SortB.Size = new System.Drawing.Size(98, 29);
+            this.SortB.TabIndex = 3;
+            this.SortB.Text = "Sort";
+            this.SortB.UseVisualStyleBackColor = true;
+            // 
+            // ASort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.SortB);
+            this.Controls.Add(this.ConvertTypes);
+            this.Controls.Add(this.PathBox);
+            this.Controls.Add(this.SelectB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 300);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "ASort";
+            this.Text = "ASort";
+            this.Shown += new System.EventHandler(this.ASort_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SelectB;
+        private System.Windows.Forms.TextBox PathBox;
+        private System.Windows.Forms.RichTextBox ConvertTypes;
+        private System.Windows.Forms.Button SortB;
+        private System.Windows.Forms.FolderBrowserDialog FolderDial;
     }
 }
 
--- ASort/Form1.cs
+++ ASort/Form1.cs
@@ -10,11 +10,34 @@ using System.Windows.Forms;
 
 namespace ASort
 {
-    public partial class Form1 : Form
+    
+    public partial class ASort : Form
     {
-        public Form1()
+        string GPath = "";
+        public ASort()
         {
             InitializeComponent();
+        }
+
+        private void SelectB_Click(object sender, EventArgs e)
+        {
+            DialogResult Bool = FolderDial.ShowDialog();
+            string Path = FolderDial.SelectedPath;
+            if (Bool == DialogResult.OK && !string.IsNullOrWhiteSpace(Path))
+            {
+                PathBox.Text = Path;
+                Upd();
+            }
+        }
+        private void Upd()
+        {
+            GPath = PathBox.Text;
+        }
+
+        private void ASort_Shown(object sender, EventArgs e)
+        {
+            PathBox.Text = Environment.CurrentDirectory;
+            Upd();
         }
     }
 }
